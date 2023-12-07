d6a7fdc4dfa6727ed87f538ed22f4a3fe512d1c3
MatthiasBenaets <matthias.benaets@gmail.com>
2023-12-07 13:51:28 +0100

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+{#if $page.status === 404}
+  <div class="mt-56 flex flex-row items-center justify-center">
+    <img class="h-40 sm:pr-10" src="/castle.svg" alt="castle" style="aspect-ratio: 1/1;">
+    <div>
+      <h1 class="text-4xl text-neutral-100">{$page.status} {$page.error.message}</h1>
+      <p class="text-neutral-400">Thank you visitor! <br> But our princess is in another castle</p>
+    </div>
+  </div>
+{:else if $page.status === 500}
+  <div class= "mt-56 flex flex-row justify-between">
+    <img class="h-40 sm:pr-10" src="/dog.svg" alt="Dog ready to fix the problem" style="aspect-ratio: 1/1;">
+    <div>
+      <h1 class="pt-10 text-4xl text-neutral-100">{$page.status} {$page.error.message}</h1>
+      <p class="text-neutral-400">Whoops. That wasn't supposed to happen!</p>
+    </div>
+  </div>
+{/if}
